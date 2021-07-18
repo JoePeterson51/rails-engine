ed1ddf53f4064743b3971236c3586852b84f53c0
JoePeterson51 <>
2021-07-17 20:20:39 -0600

    Limit merchant page to 20 merchants

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -1,5 +1,8 @@
 class Api::V1::MerchantsController < ApplicationController
-  def index
+  MERCHANTS_PER_PAGE = 20
 
+  def index
+    page = params.fetch(:page, 0).to_i
+    render json: Merchant.offset(page * MERCHANTS_PER_PAGE).limit(MERCHANTS_PER_PAGE)
   end
 end
--- spec/requests/api/v1/merchant_request_spec.rb
+++ spec/requests/api/v1/merchant_request_spec.rb
@@ -4,8 +4,18 @@ RSpec.describe 'Merchants API' do
   it 'sends all merchants in pages of 20' do
     create_list(:merchant, 40)
 
-    get '/api/v1/merchants?page=1'
+    get '/api/v1/merchants'
 
     expect(response).to be_successful
+
+    merchants = JSON.parse(response.body, symbolize_names: true)
+
+    merchants.each do |merchant|
+      expect(merchant).to have_key(:id)
+      expect(merchant[:id]).to be_an(Integer)
+
+      expect(merchant).to have_key(:name)
+      expect(merchant[:name]).to be_a(String)
+    end
   end
 end
