4ee22664b9634f5d6edb3c8f1984fecce9f86426
JoePeterson51 <>
2021-07-21 15:20:08 -0600

    Fix max price item spec

--- spec/requests/api/v1/items/search/find_by_max_price_spec.rb
+++ spec/requests/api/v1/items/search/find_by_max_price_spec.rb
@@ -3,9 +3,9 @@ require 'rails_helper'
 RSpec.describe 'find item by max price' do
   it 'can find an item by a maximum price search' do
     merchant1 = FactoryBot.create(:merchant)
-    item1 = FactoryBot.create(:item, id: 1, unit_price: 5.0, merchant: merchant1)
-    item2 = FactoryBot.create(:item, id: 2, unit_price: 7.5, merchant: merchant1)
-    item3 = FactoryBot.create(:item, id: 3, unit_price: 10.0, merchant: merchant1)
+    item1 = FactoryBot.create(:item, name: 'aaa', unit_price: 5.0, merchant: merchant1)
+    item2 = FactoryBot.create(:item, name: 'bbb', unit_price: 7.5, merchant: merchant1)
+    item3 = FactoryBot.create(:item, name: 'ccc', unit_price: 10.0, merchant: merchant1)
 
     get "/api/v1/items/find?min_price=6"
 
